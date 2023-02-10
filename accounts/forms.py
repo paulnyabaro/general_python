e339499b13d07f733ef7b0349ef6fe79f2216544
paulnyabaro <paulnyabaro1@gmail.com>
2023-02-10 14:20:59 +0300

    Updated registration form

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,7 +5,7 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age',)
+        fields = UserCreationForm.Meta.fields + ('username', 'email', 'age',)
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
