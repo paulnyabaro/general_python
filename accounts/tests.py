7794916df0bc87ac80987ad230d5052e1fbbcef2
paulnyabaro <paulnyabaro1@gmail.com>
2023-02-10 14:32:02 +0300

    Added test for url in correct location for signup

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class SignUpPateTest(TestCase):
+    def test_url_exists_at_correct_location_signupview(self):
+        response = self.client.get('/accounts/signup')
+        self.assertEqual(response.status_code, 200)
