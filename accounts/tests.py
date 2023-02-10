4bc043bdf7861a1ff26f5e974ad5d5a84f2e8a68
paulnyabaro <paulnyabaro1@gmail.com>
2023-02-10 14:39:24 +0300

    Added test for url in correct location for signup

--- accounts/tests.py
+++ accounts/tests.py
@@ -7,3 +7,8 @@ class SignUpPateTest(TestCase):
     def test_url_exists_at_correct_location_signupview(self):
         response = self.client.get('/accounts/signup/')
         self.assertEqual(response.status_code, 200)
+
+    def test_signup_view_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/signup.html')
