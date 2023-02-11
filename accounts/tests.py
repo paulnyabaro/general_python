0149f9b68e74b971228fae783511c5d939752443
paulnyabaro <paulnyabaro1@gmail.com>
2023-02-11 07:21:31 +0300

    Added sign up form test

--- accounts/tests.py
+++ accounts/tests.py
@@ -12,3 +12,17 @@ class SignUpPateTest(TestCase):
         response = self.client.get(reverse('signup'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'registration/signup.html')
+
+    def test_signup_form(self):
+        response = self.client.post(reverse('signup'),
+            {
+                'username': 'testuser',
+                'email': 'testuseremail@gmail.com',
+                'password1': 'testpass123',
+                'password2': 'testpass123',
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, 'testuser')
+        self.assertEqual(get_user_model().objects.all()[0].email, 'testuseremail@gmail.com')
