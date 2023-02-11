eea48ebb72bbbc99a8b7cba345420f5ece43d216
paulnyabaro <paulnyabaro1@gmail.com>
2023-02-11 11:05:20 +0300

    Added homepage view tests

--- pages/tests.py
+++ pages/tests.py
@@ -7,3 +7,8 @@ class HomePageTests(SimpleTestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
+    def test_homepage_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertContains(response, "Home")
