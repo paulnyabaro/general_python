a6bc85438a09c3614a0b7a468f18ba60197b3ef1
paulnyabaro <paulnyabaro1@gmail.com>
2023-02-13 07:28:35 +0300

    Added Email Backend

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -137,3 +137,6 @@ LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_ALLOWED_TEMPLATE_PACK = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
