0bd35f683b872c5b9a018dabcf8df26167da9613
paulnyabaro <paulnyabaro1@gmail.com>
2023-02-14 08:27:11 +0300

    Added delete and edit views

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, DeleteView, UpdateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView
 
 from .models import Article
 
