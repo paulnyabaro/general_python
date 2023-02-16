16a858e4a76b084529daff509bafb6dd5c730c45
paulnyabaro <paulnyabaro1@gmail.com>
2023-02-16 06:50:48 +0300

    Added login required mixin

--- articles/views.py
+++ articles/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 
 from .models import Article
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = 'article_new.html'
     fields = ('title', 'body')
@@ -14,11 +15,11 @@ class ArticleCreateView(CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = 'article_list.html'
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = 'article_detail.html'
 
