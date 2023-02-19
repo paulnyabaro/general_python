9558171471f24f9d40441ff16fd36e52fbc8cfbd
paulnyabaro <paulnyabaro1@gmail.com>
2023-02-19 07:20:35 +0300

    Hid secret key

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5a_l2c8)wi6b7-hj!r&=v3jo@b4sw!o0-&#8h2fo=p(jj)^0fd'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
